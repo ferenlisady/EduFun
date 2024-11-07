988d3e6981f3fd8188373f7ef2dedd08673041e5
Feren Lisady <ferenlisady06@gmail.com>
2024-11-08 00:46:48 +0700

    favicon

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="icon" href="{{ asset('assets/logo.png') }}" type="image/png">
     <title>@yield('title')</title>
     @include('layout.style')
     @yield('style')
